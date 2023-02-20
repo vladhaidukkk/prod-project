715f35a433d8e8703296d999e8ba8c1d49641444
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-02-20 20:44:12 +0200

    test: create html report generator script for visual regression tests

---
+++ scripts/generate-visual-json-report.ts
@@ -0,0 +1,33 @@
+import { promisify } from 'node:util';
+import { readdir, writeFile } from 'node:fs';
+import { join, relative } from 'node:path';
+
+const asyncReaddir = promisify(readdir);
+const writeFileAsync = promisify(writeFile);
+
+const lokiDir = join(__dirname, '..', '.loki');
+const actualDir = join(lokiDir, 'current');
+const expectedDir = join(lokiDir, 'reference');
+const diffDir = join(lokiDir, 'difference');
+
+async function main() {
+  const diffs = await asyncReaddir(diffDir);
+
+  await writeFileAsync(
+    join(lokiDir, 'report.json'),
+    JSON.stringify({
+      newItems: [],
+      deletedItems: [],
+      passedItems: [],
+      failedItems: diffs,
+      expectedItems: diffs,
+      actualItems: diffs,
+      diffItems: diffs,
+      actualDir: relative(lokiDir, actualDir),
+      expectedDir: relative(lokiDir, expectedDir),
+      diffDir: relative(lokiDir, diffDir),
+    }),
+  );
+}
+
+void main();
