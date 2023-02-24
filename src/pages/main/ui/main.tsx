a6f1ef02790aa3f8ea11905b613522072c24b31c
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-02-24 09:51:56 +0200

    build: setup pre-commit hook like CI and commit-msg hook

--- src/pages/main/ui/main.tsx
+++ src/pages/main/ui/main.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/error-boundary';
-import { Counter } from 'entities/counter';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
@@ -8,7 +7,6 @@ const MainPage = () => {
   return (
     <div>
       <p>{t('Main Page')}</p>
-      <Counter />
       <BugButton />
     </div>
   );
